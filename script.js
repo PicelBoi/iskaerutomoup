b5244d02ed1ea963f385d339a0b114dc886605db
PicelBoi <101955849+PicelBoi@users.noreply.github.com>
2025-04-06 16:56:39 -0400

    yes

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-// how 2 code, number 1 take code from stack overflow https://stackoverflow.com/questions/542938/how-to-calculate-number-of-days-between-two-dates/46529159#46529159
-
-var dateofdestruction = "2025-03-29";
-var fixed = true;
-var deusexmachina = "2025-04-06";
-
-// JavaScript / NodeJs answer
-var startDate = new Date(dateofdestruction);
-var endDate = new Date();
-if (fixed == true) {
-  endDate = new Date(deusexmachina);
-}
-
-var difference = endDate.getTime() - startDate.getTime();
-
-var TotalDiffDays = Math.ceil(difference / (1000 * 3600 * 24));
 document.addEventListener("DOMContentLoaded", function () {
+  // how 2 code, number 1 take code from stack overflow https://stackoverflow.com/questions/542938/how-to-calculate-number-of-days-between-two-dates/46529159#46529159
+
+  var dateofdestruction = "2025-03-29";
+  var fixed = true;
+  var deusexmachina = "2025-04-06";
+
+  // JavaScript / NodeJs answer
+  var startDate = new Date(dateofdestruction);
+  var endDate = new Date();
+  if (fixed == true) {
+    endDate = new Date(deusexmachina);
+  }
+
+  var difference = endDate.getTime() - startDate.getTime();
+
+  var TotalDiffDays = Math.ceil(difference / (1000 * 3600 * 24));
+
+  // make request (this is broken because CORS)
+  /*
+  var pingedktomo = false;
+  var ktomoup = false;
+  var ping = new XMLHttpRequest();
+  var ktomoendpoint = "https://ktp.kaeru.world/v1/player/get_player_data";
+
+  ping.open("GET", ktomoendpoint, true);
+  ping.setRequestHeader(
+    "User-Agent",
+    "Is Kaerutomo Up? PicelBoi http://iskaerutomoup.picelboi.xyz"
+  );
+  ping.onreadystatechange = function () {
+    console.log("APESHIT");
+    if (ping.readyState == XMLHttpRequest.DONE) {
+      pingedktomo = true;
+    }
+
+    if (
+      ping.readyState == XMLHttpRequest.DONE &&
+      ping.status >= 200 &&
+      ping.status < 500
+    ) {
+      ktomoup = true;
+    }
+  };
+  ping.send(); */
+
+  console.log(ping);
   document.getElementById("counter").textContent =
     "It has been " + TotalDiffDays + " days since Kaerutomo stopped working.";
 
@@ -31,5 +61,18 @@ document.addEventListener("DOMContentLoaded", function () {
       ", was fixed on " +
       deusexmachina +
       ".)";
+  }
+
+  statusElement = document.getElementById("isup");
+  asofElement = document.getElementById("asof");
+  if (pingedktomo == true) {
+    asofElement.textContent(
+      "As of now because this site just used your browser to ping Kaerutomo to see if it's up or not"
+    );
+    if (ktomoup == true) {
+      statusElement.textContent("YES!!! :D");
+    } else {
+      statusElement.textContent("NO!");
+    }
   }
 });
