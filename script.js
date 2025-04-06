af7dec03219e24850516d286a371217a513c9d97
PicelBoi <101955849+PicelBoi@users.noreply.github.com>
2025-04-06 16:14:45 -0400

    Fixed

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // how 2 code, number 1 take code from stack overflow https://stackoverflow.com/questions/542938/how-to-calculate-number-of-days-between-two-dates/46529159#46529159
 
-dateofdestruction = "2025-03-29";
+var dateofdestruction = "2025-03-29";
+var fixed = true;
+var deusexmachina = "2025-04-06";
 
 // JavaScript / NodeJs answer
 var startDate = new Date(dateofdestruction);
 var endDate = new Date();
+if (fixed == true) {
+  endDate = new Date(deusexmachina);
+}
 
 var difference = endDate.getTime() - startDate.getTime();
 
@@ -15,4 +20,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.getElementById("dateofdestruction").textContent =
     "(last broke at " + dateofdestruction + ".)";
+
+  if (fixed == true) {
+    document.getElementById("counter").textContent =
+      "Kaerutomo was down for " + TotalDiffDays + " days.";
+
+    document.getElementById("dateofdestruction").textContent =
+      "(last broke at " +
+      dateofdestruction +
+      ", was fixed on " +
+      deusexmachina +
+      ".)";
+  }
 });
